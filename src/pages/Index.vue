859de8db9adcc657f959b278138deae132d244d8
einystar <71036940+einystar@users.noreply.github.com>
2020-09-10 14:59:00 -0700

    Attempting to change journals to updates

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -35,13 +35,13 @@ query Posts {
 <script>
 import Hero from "@/components/Hero"
 import ProjectsGrid from "@/components/ProjectsGrid"
-import LatestJournals from "@/components/LatestJournals"
+import LatestUpdates from "@/components/LatestUpdates"
 
 export default {
   components: {
     Hero,
     ProjectsGrid,
-    LatestJournals
+    LatestUpdates
   }
 }
 </script>
