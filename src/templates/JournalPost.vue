804fa63e60eb9f79a70299d4842e7d9cc7f828cb
einystar <71036940+einystar@users.noreply.github.com>
2020-09-10 15:58:31 -0700

    Update JournalPost.vue

--- src/templates/JournalPost.vue
+++ src/templates/JournalPost.vue
@@ -6,12 +6,7 @@
         <div class="journal-header">
           <h1 v-html="$page.post.title" class="journal-title" />
           <div class="journal-meta">
-            <div class="journal-author">
-              <span class="label">Author</span>
-              <span class="author-name" v-text="$page.post.author" />
-            </div>
             <div class="journal-date">
-              <span class="label">Date</span>
               <div v-text="$page.post.date"/>
             </div>
           </div>          
@@ -29,7 +24,7 @@ query JournalPost ($path: String!) {
   post: journalPost (path: $path) {
     title
     author
-    date (format: "D. MMMM YYYY")
+    date (format: "MMMM D, YYYY")
     timeToRead
     content
   }
@@ -56,7 +51,7 @@ export default {
   max-width: 840px;
 }
 .journal-header {
-  padding: 2rem 0 4rem 0;
+  padding: 5rem 0 2rem 0;
 }
 .journal-title {
   font-size: 4rem;
